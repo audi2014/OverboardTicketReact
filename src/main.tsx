6a300702eff66905a228ac7c4b943c00664f4a42
Artur <artur@haulwith.us>
2022-12-28 17:28:56 +0200

    feat/code-quality-tools - play with husky

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App
-    />
-  </React.StrictMode> ,
+    <App />
+  </React.StrictMode>,
 );
