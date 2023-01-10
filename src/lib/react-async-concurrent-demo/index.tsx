199f9314393762fad8686071456ab9704d3fba0a
Artur <artur@haulwith.us>
2023-01-10 14:12:44 +0200

    no message

--- src/lib/react-async-concurrent-demo/index.tsx
+++ src/lib/react-async-concurrent-demo/index.tsx
@@ -1,43 +1,12 @@
 import { AsyncRoot } from 'lib/react-async-concurrent';
+import { AsyncTest } from 'lib/react-async-concurrent-demo/AsyncTest';
 import React, { useEffect } from 'react';
-
-import { makeAsyncTest, MakeChildrenElements } from './makeAsyncTest';
-
-/// TESTS -----------------
-
-const makeChildrenElements: MakeChildrenElements = ({
-  count,
-  chCount,
-  ms,
-  awaited,
-  asyncStart,
-  asyncChildren,
-  alwaysShowChildren,
-}) =>
-  new Array(chCount).fill(null).map((_, index) => (
-    <AsyncTestAlwaysShowChildren
-      key={index}
-      initialValues={{
-        count: count * 10 + index,
-        chCount: 0,
-        ms,
-        awaited,
-        asyncStart,
-        asyncChildren,
-        alwaysShowChildren,
-      }}
-    />
-  ));
-
-const AsyncTestAlwaysShowChildren = makeAsyncTest({
-  makeChildrenElements,
-});
 
 export const ReactAsyncConcurrentDemo = () => (
   <>
     <h1>alwaysShowChildren</h1>
     <AsyncRoot>
-      <AsyncTestAlwaysShowChildren initialValues={{ count: 2, chCount: 2 }} />
+      <AsyncTest initialValues={{ count: 2, chCount: 2 }} />
     </AsyncRoot>
   </>
 );
